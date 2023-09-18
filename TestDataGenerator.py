e051ddbe4be4d1fc787ab14e40ffcd1ed5126fd3
curtis3204 <curtis3204@gmail.com>
2023-09-19 00:01:52 +0800

    change test data to interger

--- TestDataGenerator.py
+++ TestDataGenerator.py
@@ -5,19 +5,21 @@ import random
 
 data = []
 
-ArrayLength = 20
+ArrayLength = 1000
 # Sample Number Range
-MinBoundary = -100
-MaxBoundary = 100
+MinBoundary = -100000000000000000000000
+MaxBoundary = 1000000000000000000000000
 
 DecimalBoundary = 10
 
 for i in range(ArrayLength):
     # Randomly generate a float number between 0 and 100000000000
-    np.set_printoptions(suppress=True)
+    # np.set_printoptions(suppress=True)
     
-    decimal = random.randint(0, DecimalBoundary)
-    temp = round(random.uniform(MinBoundary, MaxBoundary), decimal)
+    # decimal = random.randint(0, DecimalBoundary)
+    # temp = round(random.uniform(MinBoundary, MaxBoundary), decimal)
+    temp = random.randint(MinBoundary, MaxBoundary)
+
     data.append(temp)
 
     # data.append(random.randint(0, 100))
