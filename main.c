27b8901411c0ec9a046684dbf5d73220a26fc40d
curtis3204 <curtis3204@gmail.com>
2023-09-18 23:32:17 +0800

    add ignore

--- main.c
+++ main.c
@@ -2,16 +2,19 @@
 
 int main(void) {
   double max, min, temp;
+  printf("Enter a number: ");
   scanf("%lf", &temp);
   max = temp;
   min = temp;
   while (scanf("%lf", &temp) != EOF) {
+    printf("GO");
     if (temp > max) {
       max = temp;
     } else if (temp < min) {
       min = temp;
     }
   }
+  printf("max,min");
   printf("%lf,%lf", max, min);
   return 0;
 }
